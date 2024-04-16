f1855013b887189182f16576a4f1161fbe67513f
malveira_gbr <gabrielmalveira01@gmail.com>
2024-04-16 15:40:15 -0300

    adicionado obrigatoriedade ao preencher os campos

--- js/script.js
+++ js/script.js
@@ -1,25 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var campoTexto = document.getElementById('nome');
 
-    campoTexto.addEventListener('input', function() {
-        var texto = this.value;
-        this.value = texto.replace(/[0-9]/g, ''); // Remove todos os números da entrada do usuário
-    });
-});
-document.addEventListener('DOMContentLoaded', function() {
-    // Seleciona o campo de entrada e o botão de envio
-    var campoDeEntrada = document.getElementById('campoDeEntrada');
-    var botaoEnviar = document.getElementById('botaoEnviar');
+function verificarPreenchimentoDadosPessoais(){
+    var nome_campo = document.getElementById('nome').value
+    var email_campo = document.getElementById('email').value
+    var cpf_campo = document.getElementById('cpf').value
+    var sexo_campo = document.getElementById('sexo').value
+    var data_nascimento_campo = document.getElementById('data_nascimento').value
+    var numero_campo = document.getElementById('telefone').value
+    if(numero_campo == ''||nome_campo == '' || email_campo == '' || cpf_campo == '' || sexo_campo == '...' || data_nascimento_campo == '' ){
+        alert('Preencha as informações')
 
-    // Adiciona um ouvinte de evento ao botão de envio
-    botaoEnviar.addEventListener('click', function() {
-        // Verifica se o campo de entrada está vazio
-        if (campoDeEntrada.value === '') {
-            alert('Por favor, preencha o campo de entrada!');
-        } else {
-            // Se o campo não estiver vazio, você pode prosseguir com o envio do formulário ou outras ações
-            alert('Campo preenchido: ' + campoDeEntrada.value);
-            // Aqui você pode adicionar qualquer ação adicional que deseja executar quando o campo está preenchido
-        }
-    });
-});
+    }
+    else{
+        window.location.href = 'pagamento.html'
+    }
+
+}
+
+
+function verificarPreenchimentoDadosFinanceiros(){
+    var num_cartao_campo = document.getElementById('numero_cartao').value
+    var data_validade_campo = document.getElementById('data_validade').value
+    var cvv_campo = document.getElementById('cvv').value
+    var nome_proprietario_campo = document.getElementById('nome_proprietario').value
+    var num_parcelas_campo = document.getElementById('num_parcelas').value
+    if(num_cartao_campo == '' ||data_validade_campo == '' || cvv_campo == '' || nome_proprietario_campo == '' || num_parcelas_campo == '...' ){
+        alert('Preencha as informações')
+
+    }
+    else{
+        window.location.href = 'pagEfetuado.html'
+    }
+
+}
+
+function verificarPreenchimentoAreaCliente(){
+    var email_cliente = document.getElementById('email_cliente').value
+    var cpf_cliente = document.getElementById('cpf_cliente').value
+    
+    if(email_cliente == '' || cpf_cliente == ''){
+        alert('Preencha as informações')
+
+    }
+    else{
+        window.location.href = 'areaCliente.html'
+    }
+
+}
